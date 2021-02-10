6c191394afa2a468658d70f11318c9f53b8e4290
Irina Romjalis <iromjalis@gmail.com>
2021-02-10 02:48:09 +0200

    module 2 functions

--- js/notebook.js
+++ js/notebook.js
@@ -1,18 +1,6 @@
-function makeTask(data) {
-  const completed = false;
-  const category = "Общее";
-  const priority = "Обычный";
-
-  // Пиши код ниже этой строки
-  // Пиши код выше этой строки
+function sum() {
+  return [].reduce.call(arguments, function (a, b) {
+    return a + b;
+  });
 }
-
-const hotel = {
-  // nameOfHotel: "Resort Hotel",
-  stars: 5,
-  capacity: 100,
-};
-
-const { nameOfHotel: hotelName = "hotel", stars: hotelStars, status: hotelStatus = "empty" } = hotel; // DEFAULT
-
-// console.log(hotelName, hotelStatus); // "Resort Hotel", 5, "empty"
+sum(1, 2, 3);
